4f184e258308a5b9c09920141f7ebc99d2b33ee8
nicolasuniagustiniana <143227680+nicolasuniagustiniana@users.noreply.github.com>
2023-09-04 17:57:07 -0500

    Add files via upload

---
+++ parcial.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class parcial{
+    public static void main (String [] args){
+        int dato;
+        int exponencial;
+        
+        Scanner lectura = new Scanner(System.in); 
+     System.out.println("digite numero");
+     int ndato=lectura.nextInt();
+
+      System.out.println("digite exponencial");
+        int nexponencial=lectura.nextInt();
+
+        int exponencial1=(dato*exponencial)*dato;
+        System.out.println("la potencia "+dato+"\nnumero potencia:"+numeroexponencial+"\nes:"+exponencial);
+}
+
+}
